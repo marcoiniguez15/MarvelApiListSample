d7a7f69784e889a6c39b67ec8b93fd6870459fcd
Marco Iniguez Ollero <marco.iniguez.ollero@accenture.com>
2022-03-02 16:34:10 +0000

    Remove pods from GIT

--- MarvelAPIListSample/Pods/Target Support Files/Pods-MarvelAPIListSampleTests/Pods-MarvelAPIListSampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MarvelAPIListSampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_MarvelAPIListSampleTests
-@end
--- MarvelAPIListSample/Pods/Target Support Files/Pods-MarvelAPIListSampleUITests/Pods-MarvelAPIListSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MarvelAPIListSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MarvelAPIListSampleUITests
-@end
